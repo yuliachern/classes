8163821f583f55d5908131b0268356a0f5e53afe
yuliach <yuliachern9@gmail.com>
2024-06-25 21:46:37 +0200

    Add project files.

---
+++ classes/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        // Field member
+        private string _taxNumber;
+        public void PrintFullName()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+        public void PrintInitials()
+        {
+            var firstInitial = FirstName[0];
+            var lastInitial = LastName[0];
+            Console.WriteLine($"{firstInitial} {lastInitial}");
+        }
+        public void GenerateTaxNumber()
+        {
+            _taxNumber = RandomNumberGenerator.GetInt32(100000, 9999999).ToString();
+        }
+        public string GetTaxNumber()
+        { return _taxNumber; }
+    }
+}
---
+++ classes/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using classes;
+
+Person person; // null by default
+Person baby = new Person();
+baby.LastName = "Thompson";
+baby.FirstName = "Theresa";
+baby.DateOfBirth = new DateOnly(2023, 10, 29);
+
+
+baby.PrintFullName();
+baby.PrintInitials();
+baby.GenerateTaxNumber();
+baby.GetTaxNumber();
+
+var taxNumber = baby.GetTaxNumber();
+Console.WriteLine(taxNumber);   
+
+
+
